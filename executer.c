299e9a3e4af6556d6f2e6f74aa79924448a33de7
Mostafa.Rafiki <rafikimostafa@gmail.com>
2024-01-18 00:20:40 +0100

    add executer and splitter functions

--- executer.c
+++ executer.c
@@ -6,47 +6,47 @@
  * @stack: the list
  * Return: void
  */
- 
+
 void executer(char *string[], stack_t *stack)
 {
-    int line_num, i, j;
-    char *cmd;
-    
-    instruction_t st[] = {
-        {"pall", pall},
-        {"pint", pint},
-        {"add", add},
-        {"swap", swap},
-        {"null", NULL}
-    };
-    
-    cmd = splitter(string[]);
-    for (line_num = 1, i = 0; cmd[i]; i++, line_num++)
-    {
-        if (_strcmp("push", cmd[i]) == 0)
-            push(&stack, line_num, pushint(cmd[i], line_num));
-        else if (_strcmp("nop", cmd[i]) == 0)
-            ;
-        else
-        {
-            j = 0;
-            while (_strcmp(st[j].opcode, "null") != 0)
-            {
-                if (_strcmp(st[j].opcode, cmd[i]) == 0)
-                {
-                    st[j].f(&stack, line_num);
-                    break;
-                }
-                j++;
-            }
-            if (_strcmp(st[j].opcode, "null") == 0 && _strcmp(cmd[i], "\n") != 0)
-            {
-                fprintf(stderr, "L%d: unknown instruction %s", line_num, cmd[i]);
-                if (!num_line_finder(cmd[i]))
-                    fprintf(stderr, "\n");
-                    exit(EXIT_FAILLURE);
-            }
-        }
-    }
-    freed_stack(stack);
+	int line_num, i, j;
+	char *cmd;
+
+	instruction_t st[] = {
+		{"pall", pall},
+		{"pint", pint},
+		{"add", add},
+		{"swap", swap},
+		{"null", NULL}
+	};
+
+	cmd = splitter(string[]);
+	for (line_num = 1, i = 0; cmd[i]; i++, line_num++)
+	{
+		if (_strcmp("push", cmd[i]) == 0)
+			push(&stack, line_num, pushint(cmd[i], line_num));
+		else if (_strcmp("nop", cmd[i]) == 0)
+			;
+		else
+		{
+			j = 0;
+			while (_strcmp(st[j].opcode, "null") != 0)
+			{
+				if (_strcmp(st[j].opcode, cmd[i]) == 0)
+				{
+					st[j].f(&stack, line_num);
+					break;
+				}
+				j++;
+			}
+			if (_strcmp(st[j].opcode, "null") == 0 && _strcmp(cmd[i], "\n") != 0)
+			{
+				fprintf(stderr, "L%d: unknown instruction %s", line_num, cmd[i]);
+				if (!num_line_finder(cmd[i]))
+					fprintf(stderr, "\n");
+					exit(EXIT_FAILLURE);
+			}
+		}
+	}
+	freed_stack(stack);
 }
--- splitter.c
+++ splitter.c
@@ -3,67 +3,67 @@
 /**
  * splitter - split the string to array of char
  * @str: string to split
- * 
  * Return: array of char
 */
+
 char **splitter(char *str[])
 {
-    char *token, **tokens = NULL, *buffer = NULL;
-    int counter = 0, i = 0;
+	char *token, **tokens = NULL, *buffer = NULL;
+	int counter = 0, i = 0;
 
-    if (!str[])
-        return (NULL);
-    buffer = _strdup(str[]);
-    token = strtok(buffer, " \t\n");
-    if (token == NULL)
-    {
-    free(str[]), str[] = NULL;
-    free(buffer), buffer = NULL;
-    return (NULL);
-    }
-    while (token)
-    {
-    counter++;
-    token = strtok(NULL, " \t\n");
-    }
-    free(buffer), buffer = NULL;
-    tokens = malloc(sizeof(char *) * (counter + 1));
-    if (tokens == NULL)
-    {
-    free(str), str = NULL;;
-    return (NULL);
-    }
-    token = strtok(str, " \t\n");
-    while (token)
-    {
-    tokens[i] = _strdup(token);
-    token = strtok(NULL, " \t\n");
-    i++;
-    }
-    free (str), str = NULL;
-    tokens[i] = NULL;
-    return (tokens);
+	if (!str[])
+		return (NULL);
+	buffer = _strdup(str[]);
+	token = strtok(buffer, " \t\n");
+	if (token == NULL)
+	{
+		free(str[]), str[] = NULL;
+		free(buffer), buffer = NULL;
+		return (NULL);
+	}
+	while (token)
+	{
+		counter++;
+		token = strtok(NULL, " \t\n");
+	}
+	free(buffer), buffer = NULL;
+	tokens = malloc(sizeof(char *) * (counter + 1));
+	if (tokens == NULL)
+	{
+		free(str), str = NULL;
+		return (NULL);
+	}
+	token = strtok(str, " \t\n");
+	while (token)
+	{
+		tokens[i] = _strdup(token);
+		token = strtok(NULL, " \t\n");
+		i++;
+	}
+	free(str), str = NULL;
+	tokens[i] = NULL;
+	return (tokens);
 }
 
 /**
-* _strcmp - comp two string with n char
+* _strcmp - comparaison of two strings
 * @s1: 1st string
 * @s2: 2nd string
 *
-* Return: the comp string
+* Return: negative if s1 < s2, positive if s1 > s2,
+* zero if s1 == s2
 */
 
-int _strncmp(const char *s1, const char *s2, size_t n) 
+int _strcmp(char *s1, char *s2)
 {
-    while (n > 0 && (*s1 != '\0' || *s2 != '\0'))
-    {
-        if (*s1 != *s2)
-        {
-            return (*s1 - *s2);
-        }
-        s1++;
-        s2++;
-        n--;
-    }
-    return (0);
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	return (*s1 < *s2 ? -1 : 1);
 }
